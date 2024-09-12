6ca335a514abe069efcbd39adeb0301922c35bde
aaron-gcl-bi <94833179+aaronchrisjo@users.noreply.github.com>
2024-09-12 12:25:11 +0530

    Initial commit 🎉

---
+++ src/app/application/application.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ApplicationRoutingModule } from './application-routing.module';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent
+  ],
+  imports: [
+    CommonModule,
+    ApplicationRoutingModule
+  ],
+  exports:[
+    NavbarComponent
+  ]
+})
+export class ApplicationModule { }
