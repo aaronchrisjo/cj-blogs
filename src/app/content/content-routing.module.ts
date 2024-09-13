ae6d847401313b59c500a1f9006b79a15155bf5f
aaron-gcl-bi <94833179+aaronchrisjo@users.noreply.github.com>
2024-09-13 09:36:01 +0530

    Added fa-logos . Added an angular child component.

--- src/app/content/content-routing.module.ts
+++ src/app/content/content-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WebDevComponent } from './web-dev/web-dev.component';
+import { AngularComponent } from './web-dev/angular/angular.component';
 
 const routes: Routes = [
-  {path:'web-dev', component:WebDevComponent}
+  {path:'web-dev', component:WebDevComponent},
+  {path:'angular', component:AngularComponent}
 ];
 
 @NgModule({
--- src/app/content/content.module.ts
+++ src/app/content/content.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ContentRoutingModule } from './content-routing.module';
 import { WebDevComponent } from './web-dev/web-dev.component';
+import { AngularComponent } from './web-dev/angular/angular.component';
 
 
 @NgModule({
   declarations: [
-    WebDevComponent
+    WebDevComponent,
+    AngularComponent
   ],
   imports: [
     CommonModule,
