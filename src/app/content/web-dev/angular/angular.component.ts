f551fc6d5613e1c1b242890908bf3d9da027a3db
aaron-gcl-bi <94833179+aaronchrisjo@users.noreply.github.com>
2024-09-14 14:35:41 +0530

    Created & updated TOC

--- src/app/content/web-dev/angular/angular.component.ts
+++ src/app/content/web-dev/angular/angular.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-angular',
@@ -7,11 +8,31 @@ import { Router } from '@angular/router';
   styleUrl: './angular.component.css'
 })
 export class AngularComponent {
-  constructor(private router: Router){}
-
-  goToRoute(route: string) {
-    this.router.navigate([route]).then(() => {
-      window.scrollTo({ top: 0, behavior: 'smooth' });  
+  constructor(private router: Router) {
+    // Listen for navigation events to handle fragment scrolling
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      // Get the current fragment from the URL
+      const fragment = this.router.url.split('#')[1];
+      if (fragment) {
+        const element = document.getElementById(fragment);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth' });
+        }
+      }
     });
+  }
+
+  goToRoute(route: string, fragment?: string) {
+    this.router.navigate([route], { fragment: fragment });
+  }
+
+  goToSection(route: string, sectionId: string) {
+    this.goToRoute(route, sectionId);
+  }
+
+  goToSection1(id: string) {
+    this.router.navigate([], { fragment: id });
   }
 }
