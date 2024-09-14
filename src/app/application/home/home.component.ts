4c4ab6d84eef67fa82e2fd3f2d5421c36af4c521
aaron-gcl-bi <94833179+aaronchrisjo@users.noreply.github.com>
2024-09-14 10:42:59 +0530

    Updated home page

--- src/app/application/home/home.component.ts
+++ src/app/application/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  
+  goToRoute(url:string){
+    window.location.href = url;
+  }
 }
